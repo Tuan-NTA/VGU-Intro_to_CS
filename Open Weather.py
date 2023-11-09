4ec3c984df75f040508a7883cb15193ff52b3f63
Beyonder de GOAT <135790157+Springblade@users.noreply.github.com>
2023-11-09 14:54:51 +0700

    Open Weather.py

--- Open Weather.py
+++ Open Weather.py
@@ -1,31 +1,40 @@
 import datetime as dt
 import requests
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-API_KEY = "Your_Api_Key"
-CITY = input("Enter city name: ")
+class Weather:
+    def __init__(self, api_key: str, city: str):
+        self.BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+        self.API_KEY = api_key
+        self.CITY = city
 
-def kelvin_to_celsius_fahrenheit(kelvin):
-    celsius = kelvin - 273.15
-    fahrenheit = celsius * (9/5) + 32
-    return celsius, fahrenheit
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
-response = requests.get(url).json()
+    def kelvin_to_celsius_fahrenheit(self, kelvin: float) -> tuple:
+        celsius = kelvin - 273.15
+        fahrenheit = celsius * (9/5) + 32
+        return celsius, fahrenheit
 
-temp_kelvin = response['main']['temp']
-temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
-feels_like_kelvin = response['main']['feels_like']
-feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
-wind_speed = response['wind']['speed']
-humidity = response['main']['humidity']
-description = response['weather'][0]['description']
-sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
-sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+    def get_weather(self):
+        url = self.BASE_URL + "appid=" + self.API_KEY + "&q=" + self.CITY
+        response = requests.get(url).json()
 
-print(f"Temperature in {CITY}: {temp_celsius:.2f}°C or {temp_fahrenheit:.2f}°F")
-print(f"emperature in {CITY} feels like: {feels_like_celsius:.2f}°C or { feels_like_fahrenheit:.2f}°F")
-print(f"Humidity in {CITY}: {humidity}%")
-print(f"Wind speed in {CITY}: {wind_speed}m/s")
-print(f"General weather in {CITY}: {description}")
-print(f"Sunrise in {CITY} at {sunrise_time} local time")
-print(f"Sunset in {CITY} at {sunset_time} local time")
+        temp_kelvin = response['main']['temp']
+        temp_celsius, temp_fahrenheit = self.kelvin_to_celsius_fahrenheit(temp_kelvin)
+        feels_like_kelvin = response['main']['feels_like']
+        feels_like_celsius, feels_like_fahrenheit = self.kelvin_to_celsius_fahrenheit(feels_like_kelvin)
+        wind_speed = response['wind']['speed']
+        humidity = response['main']['humidity']
+        description = response['weather'][0]['description']
+        sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+        sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+        print(f"Temperature in {self.CITY}: {temp_celsius:.2f}°C or {temp_fahrenheit:.2f}°F")
+        print(f"Temperature in {self.CITY} feels like: {feels_like_celsius:.2f}°C or { feels_like_fahrenheit:.2f}°F")
+        print(f"Humidity in {self.CITY}: {humidity}%")
+        print(f"Wind speed in {self.CITY}: {wind_speed}m/s")
+        print(f"General weather in {self.CITY}: {description}")
+        print(f"Sunrise in {self.CITY} at {sunrise_time} local time")
+        print(f"Sunset in {self.CITY} at {sunset_time} local time")
+
+api_key = "Your_API_KEY"
+city = input("Enter city name: ")
+weather = Weather(api_key, city)
+weather.get_weather()
