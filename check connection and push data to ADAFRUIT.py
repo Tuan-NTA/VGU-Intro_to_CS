4401531afc0bddf5ddec427e26c03fd595293d46
Jerry-Fishron <135790477+Jerry-Fishron@users.noreply.github.com>
2023-11-09 23:40:49 +0700

    Update check connection and push data to ADAFRUIT.py
    connect to adafruit and publish data onto the feeds

--- check connection and push data to ADAFRUIT.py
+++ check connection and push data to ADAFRUIT.py
@@ -3,10 +3,27 @@ import time
 import sys
 from Adafruit_IO import MQTTClient
 AIO_USERNAME = "*username*"
-AIO_KEY = "*key goes here*"
+AIO_KEY = "*ada key*"
+
+class Cargo:
+    def __init__(self,cargo: str, where: str):
+        self.cargo = cargo
+        self.where = where
+
+
+    def feed_cargo(self):
+        temp = self.cargo
+        temp = temp.isnumeric() #check to see if have to convert to float
+        if temp == True:
+            self.cargo = float(self.cargo)
+        client.publish(self.where, self.cargo) #uploads data to adafruit to the feed specified
+
+
+
+
 
 def connected(client):
-    print("Server connected ...")
+    print("Server connected ...") #connect to certain switches if you want
     client.subscribe("led")
     client.subscribe("waterpump")
     client.subscribe("equation")
@@ -24,7 +41,7 @@ def message(client , feed_id , payload):
         global_equation = payload
         print(global_equation)
 
-client = MQTTClient("*username*", "*key goes here*")
+client = MQTTClient(AIO_USERNAME, AIO_KEY )
 
 client.on_connect = connected  #function pointer
 client.on_disconnect = disconnected
@@ -37,7 +54,9 @@ client.loop_background()
 #This part below pushes data to the server
 while True:
     time.sleep(5) #every 5 seconds it pushes
-    client.publish("*the feed*", "*the data*")
-    client.publish("sensor2", "I did it!!!")
+    cargo = input("Enter data: ") #Data gets supplied here
+    where = input("Enter what feed: ") #The feed it goes to goes here
+    Load = Cargo(cargo, where)
+    Load.feed_cargo()
     #You can keep doing this as much as you want
     pass
