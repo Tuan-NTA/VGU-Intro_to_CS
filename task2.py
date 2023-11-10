26d4b0638423877a988dc81ce63622a0954067f7
AwesomeUser123 <135790626+AwesomeUser123@users.noreply.github.com>
2023-11-10 12:08:46 +0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -8,8 +8,6 @@ class Task2:
 
         
     def Task2_Run(self):
-        # Retrieve data from the shared data store (Task1's data)
-        data_from_task1 = self.scheduler.shared_data.get("task1_data", "No data available")
         if self.scheduler.inp_status[0] == 1:
             if self.scheduler.userinput=="":
                 self.chatbot_app.Insert_response("Please enter something")
@@ -19,6 +17,3 @@ class Task2:
             self.chatbot_app.Insert_response(response)
             self.chatbot_app.submit_button.config(state=tk.NORMAL)
             self.scheduler.inp_status[0] = 0
-
-        print("Data received by Task2:", data_from_task1)
-        print("Task 2 is activated!!!!\n")
