0617b583e8f6d2f6121234604a4c2ad3da8e93e7
Tuan-NTA <135790241+Tuan-NTA@users.noreply.github.com>
2023-11-13 19:07:37 +0700

    Add files via upload

--- VirtualAssistant/chatbot.py
+++ VirtualAssistant/chatbot.py
@@ -1,27 +1,26 @@
 import requests
-from speech2text import main
-from get_path import model_path0
 
-from rasa.cli.run import run
 
-def deploy_rasa_chatbot(model_path, endpoints_path, user_input):
-    # Run Rasa server with specified model and endpoints
-    run(model=model_path, endpoints=endpoints_path)
+def send_message_to_rasa(message):
+    url = "http://_______:5005/webhooks/rest/webhook"
+    payload = {"message": message}
+    headers = {"Content-Type": "application/json"}
 
-if __name__ == '__main__':
-    # Replace these paths with the actual paths to your trained model and endpoints configuration
-    model_path = model_path0
-    endpoints_path = ""http://localhost:5005/webhooks""
+    try:
+        response = requests.post(url, json=payload, headers=headers).json()
+        # Check if the response is not empty
+        if response:
+            return response[0]['text']
+        else:
+            return "No response from the bot."
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-    # Simulate the speech-to-text output (replace this with your actual implementation)
-    user_input = "main()"
 
-    # Pass the user's input to the Rasa chatbot
-    if user_input:
-        deploy_rasa_chatbot(model_path, endpoints_path, user_input)
-
-# Pass the text to Rasa
-rasa_response = send_message_to_rasa(main())
-
-# Handle Rasa's response as needed
-print(rasa_response)
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+        bot_response = send_message_to_rasa(user_input)
+        print(f"Bot: {bot_response}")
