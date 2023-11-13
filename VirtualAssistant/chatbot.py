af6a394f847026d1d37b3f57a373384091af2a06
Tuan-NTA <135790241+Tuan-NTA@users.noreply.github.com>
2023-11-13 19:15:31 +0700

    Add files via upload

--- VirtualAssistant/chatbot.py
+++ VirtualAssistant/chatbot.py
@@ -1,26 +1,32 @@
 import requests
 
+class RasaBot:
 
-def send_message_to_rasa(message):
-    url = "http://_______:5005/webhooks/rest/webhook"
-    payload = {"message": message}
-    headers = {"Content-Type": "application/json"}
+    def __init__(self, url="http://local/webhooks/rest/webhook"):
+        self.url = url
+        self.headers = {"Content-Type": "application/json"}
 
-    try:
-        response = requests.post(url, json=payload, headers=headers).json()
-        # Check if the response is not empty
-        if response:
-            return response[0]['text']
-        else:
-            return "No response from the bot."
-    except Exception as e:
-        return f"Error: {str(e)}"
+    def send_message(self, message):
+        payload = {"message": message}
+
+        try:
+            response = requests.post(self.url, json=payload, headers=self.headers).json()
+            # Check if the response is not empty
+            if response:
+                return response[0]['text']
+            else:
+                return "No response from the bot."
+        except Exception as e:
+            return f"Error: {str(e)}"
 
 
 if __name__ == "__main__":
+    rasa_bot = RasaBot()
+
     while True:
         user_input = input("You: ")
         if user_input.lower() == 'exit':
             break
-        bot_response = send_message_to_rasa(user_input)
+
+        bot_response = rasa_bot.send_message(user_input)
         print(f"Bot: {bot_response}")
