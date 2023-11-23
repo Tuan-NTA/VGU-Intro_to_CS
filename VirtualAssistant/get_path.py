8724251a556e10b6de5e0e3e252dddd042d186db
Tuan-NTA <135790241+Tuan-NTA@users.noreply.github.com>
2023-11-23 19:11:54 +0700

    Delete VirtualAssistant/get_path.py

--- VirtualAssistant/get_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Get the base directory of the script
-base_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Now you can use the base_directory variable to construct paths relative to the script
-model_path0 = os.path.join(base_directory, "models", "model20211201-120000")
-
-
